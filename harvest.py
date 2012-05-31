accfdc9092b1b41663d3bc9d25db9a7d8335a6e8
Oleg Salionov <o.salionov@zmeke.com>
2012-05-31 20:22:20 +1100

    working

--- harvest.py
+++ harvest.py
@@ -9,23 +9,21 @@ def loadConfig():
 
 def printResponse(row,format):
 	if format == "json":
-		print "{connections:"+row[0]+",cpu:"+row[1]+",pio:"+row[2]+",musage:"+row[3]+"}"
+		print "{connections:" + str(row[0]) + ",cpu:" + str(row[1]) + ",pio:" + str(row[2]) + ",musage:" + str(row[3]) + "}"
 	if format == "cvs":
-		print row[0] + ";" +  row[1] + ";" + row[2] + ";" + row[3]
+		print row[0] + ";" +  str(row[1]) + ";" + str(row[2]) + ";" + str(row[3])
 
 config = loadConfig()
-try:
-	msConnection = pymssql.connect(host=config.get("mssql","host"),user=config.get("mssql","user"),password=config.get("mssql","password"))
-	msCursor = msConnection.cursor()
-except:
-	print "getCursor exception! Check the config.ini for correct properties!"
+msConnection = pymssql.connect(host=config.get("mssql","host"),user=config.get("mssql","login"),password=config.get("mssql","password"))
+msCursor = msConnection.cursor()
 
 query = "select (select count(dbid) from sys.sysprocesses where dbid>0) as totalCon, sum (cpu) as cpu, sum(physical_io)  as pio, sum(memusage)  as musage from sys.sysprocesses;"	
-
-result = msCursor.execute(query)
-
-for row in result:
-	printResponse(row, config.get("commin","format"))
-
+msCursor.execute(query)
+result = msCursor.fetchall()
+if result <> None:
+	for row in result:
+		printResponse(row, config.get("common","format"))
+else:
+	print "query returs none"
 msConnection.close()
 
